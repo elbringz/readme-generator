7ff9979cd4b9bdc15b67b24519d8af47ae5d252f
Zachary Elbring <elbringzach@gmail.com>
2023-11-26 12:41:13 -0600

    Created questions and completed init function.

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your project title?',
+
+    'Describe your project.',
+
+    'What are the installation requirements for your project?',
+    
+    'What is your project used for?',
+
+    'Which license does your project have?',
+
+    'Any credits to be given?',
+
+    'How can your project be tested?',
+
+    'Add your Github URL.',
+
+    'Add your Github username.',
+
+    'What is your email address?',
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+    err ? console.error(err) : console.log('Success!'))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'title',
+        message: questions[0]
+        },
+    ])
+}
 
 // Function call to initialize app
 init();
