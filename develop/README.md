38665737443fca1bb603bbd26ae815e27c8b44dd
Zachary Elbring <elbringzach@gmail.com>
2023-11-29 16:21:23 -0600

    Completed license generating functions and fixed var names.

---
+++ develop/README.md
@@ -0,0 +1,35 @@
+# 
+
+## Description
+
+
+## Table of Contents
+[Installation](#installation)
+[Credits](#credits)
+[Usage](#usage)
+[License](#license)
+[Contact info](#contact-info)
+
+## Installation
+
+
+## Usage
+
+
+## Credits
+
+
+## License
+MIT
+[(https://choosealicense.com/licenses/MIT)]
+
+## Badges
+[![License](https://img.shields.io/badge/MIT-lightblue.svg)]
+
+## Contact-info
+
+
+
+
+## Tests
+
--- develop/generateMarkdown.js
+++ develop/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
     if(license) {
-        return '[![License](https://img.shields.io/badge/'+license+'-lightblue.svg)](https://www.boost.org/'+license+')';
+        return '[![License](https://img.shields.io/badge/'+license+'-lightblue.svg)]';
     } else if(!license) {
         return '';
     }
@@ -10,18 +10,30 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if(license) {
+        return '[(https://choosealicense.com/licenses/'+license+')]';
+    } else if(!license) {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if(license) {
+        return '## License';
+    } else if(!license) {
+        return '';
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
 ## Description
-${data.description}
+${data.desc}
 
 ## Table of Contents
 [Installation](#installation)
@@ -39,8 +51,9 @@ ${data.usage}
 ## Credits
 ${data.credits}
 
-## License
+${renderLicenseSection(data.license)}
 ${data.license}
+${renderLicenseLink(data.license)}
 
 ## Badges
 ${renderLicenseBadge(data.license)}
